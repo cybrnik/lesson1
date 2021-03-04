f1235b5d92c423ab4d13b5b8eec4cfbd6cf2a0d0
Nikita <telegramomg@ya.ru>
2021-03-04 13:22:14 +0300

    Task number 1 has been done.

--- les1/les1/HW1.swift
+++ les1/les1/HW1.swift
@@ -8,3 +8,30 @@
 import Foundation
 
 
+
+class HW1 {
+    func solution(a: Double, b: Double, c: Double) {
+        
+        let discriminant = b * b - (4 * a * c)
+        if discriminant > 0
+        {
+            
+            let x1 = ((-b+sqrt(discriminant))/(2*a))
+            let x2 = ((-b-sqrt(discriminant))/(2*a))
+        
+            print("Solution 1 is: " + String(x1))
+            print("Solution 2 is: " + String(x2))
+    
+        }
+        else if discriminant < 0{
+            print("0 Solutions")
+        }
+        else{
+            let x = ((-b+sqrt(discriminant))/(2*a))
+        
+            print("Solution is: " + String(x))
+
+        }
+    }
+        
+}
--- les1/les1/main.swift
+++ les1/les1/main.swift
@@ -9,6 +9,9 @@ import Foundation
 
 let hw2 = HW2()
 hw2.about_triangle(a: 1,b: 3)
-
+print("-----------------------------------------------------------")
 let hw3 = HW3()
 hw3.how_many_money(contribution: 100, percent: 10)
+print("-----------------------------------------------------------")
+let hw1 = HW1()
+hw1.solution(a: 2, b: 3, c: -2)
